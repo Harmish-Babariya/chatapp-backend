c419b854b1d2cd98fea562151aa93690d3827dc0
harmish babariya <harmishbabariya@gmail.com>
2023-01-23 20:37:19 +0530

    commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const { addUser, removeUser, getUser, getUsersInRoom} = require('./users');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 const router = require('./router');
 
