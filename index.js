e852945490a0bd48fcec0c4da360e29ff8db38b0
harmish babariya <harmishbabariya@gmail.com>
2023-01-26 22:00:36 +0530

    cors sol2

--- index.js
+++ index.js
@@ -49,5 +49,19 @@ io.on('connection', (socket) => {
 
 app.use(router);
 app.use(cors());
+app.use((req,res) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+}
+)
 
 server.listen(PORT, () => console.log(`Server is running on Port ${PORT}..`));
